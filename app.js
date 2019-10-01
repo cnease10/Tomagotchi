97b71daa902c26bd77975c1c6e5c813ddac006f7
Cierra Nease <45543866+cnease10@users.noreply.github.com>
2019-10-01 15:01:22 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+class Tomo {
+	constructor(hunger, sleepiness, boredom, age) {
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+		this.age = age;
+	}
+	createTomo() {
+		
+	}
+	feed(){
+
+	}
+	sleepyTime(){
+
+	}
+	play(){
+
+	}
+	nameIt() {
+
+	}
+
+};
+
+const first = new Tomo(0, 0, 0, 1);
+first.createTomo();
+
+console.log(first);
