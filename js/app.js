ebd6a1872e2c9e25d0487ab168e0d7e0015c757a
cnease10 <cierra.nease10@gmail.com>
2019-10-01 17:10:27 -0600

    fixed jQuery script issue

--- js/app.js
+++ js/app.js
@@ -5,25 +5,33 @@ class Tomo {
 		this.boredom = boredom;
 		this.age = age;
 	}
+};
+const game = {
+
 	createTomo() {
-		
-	}
+		let img = $('<img/>'); 
+		img.attr('src', 'https://vignette.wikia.nocookie.net/ideas/images/e/ef/Dory-FN.png/revision/latest/scale-to-width-down/220?cb=20140706202825');
+		$('.tomoContainer').append(img);
+	},
 	feed(){
 
-	}
+	},
 	sleepyTime(){
 
-	}
+	},
 	play(){
 
-	}
+	},
 	nameIt() {
 
-	}
+	},
+	morphism() {
 
+	},
 };
 
+
 const first = new Tomo(0, 0, 0, 1);
-first.createTomo();
+game.createTomo();
 
 console.log(first);
