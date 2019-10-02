e94138066cba9b4d100cf37101c59a7952d91e50
cnease10 <cierra.nease10@gmail.com>
2019-10-01 18:51:33 -0600

    have two timers, and two buttons working

--- js/app.js
+++ js/app.js
@@ -1,37 +1,129 @@
+//alert('Please enter a name, then hit PLAY BALL button to begin');
+$('#playGame').on('click', () => {
+dory.createTomo();
+dory.nameIt();
+dory.setHungerInterval();
+dory.setSleepyInterval();
+});
+
+$('#feed').on('click', () => {
+dory.feed();
+});
+
+$('#lights').on('click', () => {
+dory.sleepyTime();
+});
+
+$('#play').on('click', () => {
+//first.createTomo();
+});
+
+// const $sleepy = $('#sleepy');
+// 		const $bored = $('#bored');
+// 		const $age = $('#age');
+
+// 		const interval = 
+
 class Tomo {
-	constructor(hunger, sleepiness, boredom, age) {
-		this.hunger = hunger;
-		this.sleepiness = sleepiness;
-		this.boredom = boredom;
-		this.age = age;
+//hunger, sleepiness, boredom, age
+
+//this.hunger = hunger;
+		//this.sleepiness = sleepiness;
+		//this.boredom = boredom;
+		//this.age = age;
+	constructor() {
+		this.hunger = 0;
+		this.sleepiness = 0;
+		this.boredom = 0;
+		this.age = 0;
+
 	}
-};
-const game = {
 
+	setHungerInterval() {
+		const $hunger = $('#hungry');
+
+		const interval = setInterval(() => {
+
+			if (this.hunger == 10) {
+			clearInterval(interval);
+
+			} else {
+				this.hunger++
+			}
+
+		$hunger.text(`Hunger: ${this.hunger}`)
+			
+		}, 2500)
+		
+	}
+	setSleepyInterval() {
+		const $sleepy = $('#sleepy');
+
+		const interval = setInterval(() => {
+
+			if (this.sleepiness == 10) {
+			clearInterval(interval);
+
+			} else {
+				this.sleepiness++
+			}
+
+		$sleepy.text(`Sleepiness: ${this.sleepiness}`)
+			
+		}, 3500)
+		
+	}
 	createTomo() {
-		let img = $('<img/>'); 
-		img.attr('src', 'https://vignette.wikia.nocookie.net/ideas/images/e/ef/Dory-FN.png/revision/latest/scale-to-width-down/220?cb=20140706202825');
-		$('.tomoContainer').append(img);
-	},
+		if (this.age < 6) {
+			let img = $('<img/>'); 
+			img.attr('src', 'http://clipartmag.com/image/nemo-and-dory-drawing-7.jpg');
+			img.css('width', '300px');
+			img.css('height, 300px');
+			$('.tomoContainer').append(img);
+		} else if (this.age >= 6) {
+			let img = $('<img/>'); 
+			img.attr('src', 'https://vignette.wikia.nocookie.net/ideas/images/e/ef/Dory-FN.png/revision/latest/scale-to-width-down/220?cb=20140706202825');
+			$('.tomoContainer').append(img);
+		}
+	
+	}
 	feed(){
+		$('#hunger').text(`Hunger: ${this.hunger -= 1}`)
 
-	},
+	}
 	sleepyTime(){
-
-	},
+		$('#sleepy').text(`Sleepiness: ${this.sleepiness -= 1}`)
+	}
 	play(){
 
-	},
+	}
 	nameIt() {
+		const $name = $('#name').text();
+		$('.tomoContainer').append($name);
 
-	},
-	morphism() {
-
-	},
+	}
+	
+	deadTomo() {
+		if (this.hunger == 10) {
+			alert('Hunger has reached deadly levels!');
+			$('.tomoContainer').remove();
+		} else if (this.sleepiness == 10) {
+			alert('Sleepiness has reached deadly levels!');
+			$('.tomoContainer').remove();
+		} else if (this.boredom == 10) {
+			alert('Boredom has reached deadly levels!');
+			$('.tomoContainer').remove();
+		}
+	}
 };
 
 
-const first = new Tomo(0, 0, 0, 1);
-game.createTomo();
+	
 
-console.log(first);
+
+
+const dory = new Tomo ();
+console.log(dory);
+
+
+
