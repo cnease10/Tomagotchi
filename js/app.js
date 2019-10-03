ccf99eff718e6d80a5f1cfd06f13c4d96bb622cd
cnease10 <cierra.nease10@gmail.com>
2019-10-03 09:53:30 -0600

    edited out unnecessary comments & fixed indentation

--- js/app.js
+++ js/app.js
@@ -1,26 +1,24 @@
 alert('Please enter a name, then hit the Just START Swiming button to begin');
+
 $('#playGame').on('click', () => {
-dory.nameIt();
-dory.setAgeInterval();
-dory.setHungerInterval();
-dory.setSleepyInterval();
-dory.setBoredInterval();
-dory.agingTomo();
-// dory.animateex();
-// dory.animateback();
-//dory.deadTomo();
+	dory.nameIt();
+	dory.setAgeInterval();
+	dory.setHungerInterval();
+	dory.setSleepyInterval();
+	dory.setBoredInterval();
+	dory.agingTomo();
 });
 
 $('#feed').on('click', () => {
-dory.feed();
+	dory.feed();
 });
 
 $('#lights').on('click', () => {
-dory.sleepyTime();
+	dory.sleepyTime();
 });
 
 $('#play').on('click', () => {
-dory.play();
+	dory.play();
 });
 
 class Tomo {
@@ -38,14 +36,6 @@ class Tomo {
 		$h1.css('color', 'white');
 		$('.tomoContainer').append($h1);
 
-		// if (this.age == 1) {
-		// 	const $name = $('#name').val();
-		// 	const $h1 = $('<h1/>');
-		// 	$h1.text(`${$name} is here!`);
-		// 	$('.tomoContainer').append($h1);
-		// }
-
-
 	}
 
 	setHungerInterval() {
@@ -54,13 +44,9 @@ class Tomo {
 		const interval = setInterval(() => {
 
 			if (this.hunger === 10) {
-			alert('Hunger has reached deadly levels! ');
-			clearInterval(interval);
-			location.reload();
-			//this.boredom = 0;
-			// this.hunger = 0;
-			// this.sleepiness =0;
-			//this.age =0;
+				alert('Hunger has reached deadly levels!');
+				clearInterval(interval);
+				location.reload();
 
 			} else {
 				this.hunger++
@@ -77,9 +63,9 @@ class Tomo {
 		const interval = setInterval(() => {
 
 			if (this.sleepiness === 10) {
-			alert('Sleepiness has reached deadly levels!');
-			clearInterval(interval);
-			location.reload();
+				alert('Sleepiness has reached deadly levels!');
+				clearInterval(interval);
+				location.reload();
 			// this.boredom = 0;
 			//this.hunger = 0;
 			//this.sleepiness =0;
@@ -101,9 +87,9 @@ class Tomo {
 		const interval = setInterval(() => {
 
 			if (this.boredom === 10) {
-			alert('Boredom has reached deadly levels! ');
-			clearInterval(interval);
-			location.reload();
+				alert('Boredom has reached deadly levels! ');
+				clearInterval(interval);
+				location.reload();
 			//this.boredom = 0;
 			//this.hunger = 0;
 			//this.sleepiness =0;
@@ -128,9 +114,6 @@ class Tomo {
 				alert('CONGRATULATIONS! You have reached a fully aged Tomagotchi! Please refresh the browser to play again.')
 				clearInterval(interval);
 				location.reload();
-				// this.boredom = 0;
-				// this.hunger = 0;
-				// this.sleepiness =0;
 			} else {
 				this.getPics();
 				this.age++
@@ -147,29 +130,29 @@ class Tomo {
 		getPics() {
 			console.log('hi');
 			 if (this.age == 0) {
-			const img = $('<img/>'); 
-			img.addClass('fishPic');
-			img.attr('src', 'http://hyveephoto.com/images250_/dori-png-11.png');
-			img.css('width', '200px');
-			img.css('height, 200px');
+				const img = $('<img/>'); 
+				img.addClass('fishPic');
+				img.attr('src', 'http://hyveephoto.com/images250_/dori-png-11.png');
+				img.css('width', '200px');
+				img.css('height, 200px');
 
 			
-			 $('.tomoContainer').append(img);
+			 	$('.tomoContainer').append(img);
    			
    			
 		} else if (this.age == 6) {
-			$('.tomoContainer').empty();
-			const $name = $('#name').val();
-			const $h1 = $('<h1/>');
-			$h1.text(`${$name} has morphed!`)
-			$h1.css('color', 'white')
-			$('.tomoContainer').append($h1);
-			const img = $('<img/>');
-			img.addClass('fishPic');
+				$('.tomoContainer').empty();
+				const $name = $('#name').val();
+				const $h1 = $('<h1/>');
+				$h1.text(`${$name} has morphed!`)
+				$h1.css('color', 'white')
+				$('.tomoContainer').append($h1);
+				const img = $('<img/>');
+				img.addClass('fishPic');
 
-			img.attr('src', 'https://www.stickpng.com/assets/thumbs/58f37952a4fa116215a9241f.png');
-			img.css('height', '250px');
-			img.css('width', '250px');
+				img.attr('src', 'https://www.stickpng.com/assets/thumbs/58f37952a4fa116215a9241f.png');
+				img.css('height', '250px');
+				img.css('width', '250px');
 			
  
 			$('.tomoContainer').append(img);
@@ -194,23 +177,6 @@ class Tomo {
 		$('#age').text(`Age: ${this.age}`)
 	}
 	
-
-
-
-	// deadTomo() {
-	// 	if (this.hunger === 10) {
-	// 		alert('Hunger has reached deadly levels!');
-	// 		$('.tomoContainer').remove();
-	// 	} else if (this.sleepiness === 10) {
-	// 		alert('Sleepiness has reached deadly levels!');
-	// 		$('.tomoContainer').remove();
-	// 	} else if (this.boredom === 10) {
-	// 		alert('Boredom has reached deadly levels!');
-	// 		$('.tomoContainer').remove();
-	// 	}
-	// }
-
-    
 };
 
 
